701c4959a424ebcf2ef6b241f1ddc4442ba7c917
Maryline-Bellanger <maryline-bellanger_student2021@wilder.school>
2021-08-04 09:51:56 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HeroList from './components/HeroList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeroList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Hero';
+
+function HeroList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeroList;
